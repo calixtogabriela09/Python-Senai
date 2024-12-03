b850146ad428fba69e3af646b464793f5580d882
calixtogabriela09 <gabrielasantoscalixto@gmail.com>
2024-12-03 09:55:39 -0300

    Add files via upload

---
+++ Aula02.py
@@ -0,0 +1,11 @@
+print('tô com sono')
+
+nome_usuario ='Gabriela Calixto' # tipo -> str = string
+idade_usuario = 19 # tipo -> int = integer
+altura_usuario = 1.60 # tipo -> float = ponto flutuante
+maior_idade = True  # tipo -> bool = boolean
+
+print('Seja Bem-vindo', nome_usuario)
+print('sua idade é', idade_usuario, 'anos')
+print('sua altura é', altura_usuario, 'metros')
+print('você é maior de idade?', maior_idade)
